603c6c955cb9d517c9045c4205532010eca7d0c9
strelda <strelecekja@gmail.com>
2021-04-27 16:25:23 +0200

    init

---
+++ functions.js
@@ -0,0 +1,93 @@
+
+function setValue(value_element) {
+    var id;
+    var decimals = 0;
+    if (typeof(value_element) == "string") {
+        id = value_element;
+    } else {
+        id = value_element.id;
+        decimals = value_element.decimals ? value_element.decimals : decimals;
+    }
+    var value = data[id];
+
+    $('#' + id).text(parseFloat(value).toFixed(decimals));
+}
+
+function setState(state_element) {
+    var run = data[state_element.run_id];
+    var e = $('#' + state_element.id);
+    if (!run) {
+        e.text('');
+    } else {
+        if (state_element.load2_id) {
+            if (data[state_element.load2_id]) {
+                e.text('run/load 100%');
+            } else if (data[state_element.load_id]) {
+                e.text('run/load 50%');
+            } else {
+                e.text('run');
+            }
+        } else {
+            if (data[state_element.load_id]) {
+                e.text('run/load')
+            } else {
+                e.text('run');
+            }
+        }
+    }
+}
+
+function setAlarm(alarm_element) {
+    var alarm = data[alarm_element.id];
+    alarm = alarm_element.reversed ? !alarm : alarm;
+    var e = $('#' + alarm_element.id);
+    if (alarm) {
+        e.text(alarm_element.text);
+    } else {
+        e.text(alarm_element.off_text ? alarm_element.off_text : '');
+    }
+}
+
+function refreshValueElements() {
+    for (var i = 0; i < value_elements.length; i++) {
+        var id = value_elements[i];
+        setValue(id);
+    }
+}
+
+function refreshStateElements() {
+    for (var i = 0; i < state_elements.length; i++) {
+        var se = state_elements[i];
+        setState(se);
+    }
+}
+
+function refreshAlarmElements() {
+    for (var i = 0; i < alarm_elements.length; i++) {
+        var reversed = false;
+        var ae = alarm_elements[i];
+        if (ae.reversed)
+            reversed = true;
+        setAlarm(ae);
+    }
+}
+
+function refresh() {
+    refreshValueElements();
+    refreshStateElements();
+    refreshAlarmElements();
+}
+
+// function for loading data
+function loadData() {
+    $.getJSON(data_file, null, function(d) {
+        data = d;
+        refresh();
+    });
+}
+
+$(function() {
+    loadData();
+    // refresh every 5 sec
+    setInterval(loadData, 5000);
+});
